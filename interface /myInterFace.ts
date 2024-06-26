0352262dc6adae754a700457331c027d7761a802
wcoder547 <mw5667155@gmail.com>
2024-06-26 13:14:09 +0500

    add interface

---
+++ interface /myInterFace.ts
@@ -0,0 +1,31 @@
+console.log("hello world");
+
+interface user {
+  readonly userid: number;
+  name: string;
+  id: number;
+  google_id?: boolean;
+  //   startTrail: () => string;
+  startTrail(): string;
+  getCoupon(couponName: string): number;
+}
+
+interface user {
+  githubToken: string;
+}
+
+interface admin extends user {
+  role: "Fullstack" | "front-end" | "back-end";
+}
+const waseem: user = {
+  userid: 21,
+  name: "waseemakram",
+  id: 2211,
+  githubToken: "github",
+  startTrail: () => {
+    return "trail started";
+  },
+  getCoupon(name: "disc0101") {
+    return 12;
+  },
+};
