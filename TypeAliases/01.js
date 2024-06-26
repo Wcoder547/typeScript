0422d406b77f82c70b9b82f4e30c91cb3bfcfe17
wcoder547 <mw5667155@gmail.com>
2024-06-26 13:15:35 +0500

    add Type Aliases

--- TypeAliases/01.js
+++ TypeAliases/01.js
@@ -1,1 +1,9 @@
 console.log("waseem akram");
+function createUser(user) {
+    return {
+        name: "hafiz Fahad Iqbal",
+        email: "fahad@fahad.com",
+        isFahadChoco: true,
+    };
+}
+createUser({ name: "waseemakram", email: "mw@wasim.pk", isActive: true });
