2f76e82ba3ce44cadf13acd31cddec8fa452c2af
wcoder547 <mw5667155@gmail.com>
2025-06-18 09:50:12 +0500

    feat: add TypeScript array examples and configuration

---
+++ Array/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const numbers = [1, 2, 3, 4, 5];
+const numbers1 = new Array(1, 2, 3, 4, 5);
+const names = Array.of("Alice", "Bob", "Charlie");
+console.log(names[1]);
---
+++ Array/index.ts
@@ -0,0 +1,7 @@
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+const numbers1: number[] = new Array(1, 2, 3, 4, 5);
+
+const names: string[] = Array.of("Alice", "Bob", "Charlie");
+
+console.log(names[1]);
